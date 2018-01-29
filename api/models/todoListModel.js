33fa177fe69d2280ea04b5b7ea4634b857b84cf7
randy gylling <rgylling@gpstrackit.net>
2018-01-29 09:59:48 -0800

    sample task api

--- api/models/todoListModel.js
+++ api/models/todoListModel.js
@@ -1,9 +1,9 @@
 'use strict';
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 
-var TaskSchema = new Schema({
+const TaskSchema = new Schema({
   name: {
     type: String,
     required: 'Kindly enter the name of the task'
